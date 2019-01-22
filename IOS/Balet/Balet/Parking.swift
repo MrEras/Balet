935c7042ede315b56f25a92f6e30ff137a266798
Unknown <memo.romo.2095@gmail.com>
2019-01-21 18:03:15 -0600

    Lista categorias
    Chido?

---
+++ IOS/Balet/Balet/Parking.swift
@@ -0,0 +1,32 @@
+import Foundation
+import UIKit
+
+class Parking{
+    var URLImg: String?
+    var name: String?
+    var description: String?
+    var disp: Int = 0
+    
+    init(webURL inURL: String, name inName: String, desc inDesc: String,_ dispo:Int? = 0) {
+        self.URLImg = inURL
+        self.name = inName
+        self.description = inDesc
+        self.disp = dispo!
+    }
+}
+
+class ParkingCell : UITableViewCell{
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var nameLab: UILabel!
+    @IBOutlet var descLab: UILabel!
+    @IBOutlet var dispLab: UILabel!
+    
+    override func awakeFromNib() {super.awakeFromNib()}
+    
+    func configureCell(_ park: Parking){
+        imgView.downloadedFrom(link: park.URLImg!)
+        nameLab.text = "Nombre Est: " + park.name!
+        descLab.text = "Descripción: " + park.description!
+        dispLab.text = "Hay \(park.disp) lugares disponibles"
+    }
+}
--- IOS/Balet/Balet/SideMenu.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SideMenu.swift
-//  Balet
-//
-//  Created by Guillermo Romo Jiménez on 06/12/18.
-//  Copyright © 2018 memoromoj. All rights reserved.
-//
-
-import Foundation
