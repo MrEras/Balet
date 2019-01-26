03290fe983091bea789a933f88327d79b98660a0
alejandroalcaraz <aleexalcaraz@gmail.com>
2019-01-26 19:50:36 +0100

    Changed everything, added list to the parkings

--- Android/app/src/main/java/com/alas/balet/MainActivity.java
+++ Android/app/src/main/java/com/alas/balet/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
@@ -18,38 +17,26 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.alas.balet.com.alas.ListAdapters.ParkingsAdapter;
 
-public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener, OnMapReadyCallback {
-    private GoogleMap mMap;
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private FusedLocationProviderClient mFusedLocationClient;
+public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggleBar;
     private NavigationView navigationView;
     private Dialog locationDialog;
-    private double latitude;
-    private double longitude;
+    ListView list;
+    String[] names = {"Alejandro","Memo","Eras"};
+    String[] images = {"https://www.uwgb.edu/UWGBCMS/media/Maps/images/map-icon.jpg","https://www.uwgb.edu/UWGBCMS/media/Maps/images/map-icon.jpg","https://www.uwgb.edu/UWGBCMS/media/Maps/images/map-icon.jpg"};
+    String[] descriptions = {"Dios","Puto","Gay"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         drawerLayout = findViewById(R.id.activity_main);
         toggleBar = new ActionBarDrawerToggle(this, drawerLayout,R.string.Open, R.string.Close);
 
@@ -60,20 +47,24 @@ public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
         getSupportActionBar().setCustomView(R.layout.nav_bar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        ParkingsAdapter adapter=new ParkingsAdapter(MainActivity.this, names, images,descriptions);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
 
-        final Button btnSendLocation = findViewById(R.id.btnSendLocation);
-        locationDialog = new Dialog(this);
-        locationDialog.setContentView(R.layout.location_dialog);
-        final Button btnUnderstood = locationDialog.findViewById(R.id.btnUnderstood);
 
-        locationDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        locationDialog.show();
 
-        btnUnderstood.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                locationDialog.dismiss();
-            }
-        });
+//        locationDialog = new Dialog(this);
+//        locationDialog.setContentView(R.layout.location_dialog);
+//        final Button btnUnderstood = locationDialog.findViewById(R.id.btnUnderstood);
+
+//        locationDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//        locationDialog.show();
+
+//        btnUnderstood.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                locationDialog.dismiss();
+//            }
+//        });
 
         navigationView = findViewById(R.id.nv);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -104,15 +95,6 @@ public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
             }
         });
 
-        btnSendLocation.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Mandar: "+latitude+" ,"+longitude, Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getBaseContext(), GetLocation.class);
-                String a = (new LatLng(latitude, longitude).toString());
-                intent.putExtra("location", a);
-                startActivity(intent);
-            }
-        });
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -122,67 +104,4 @@ public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        askForLocationPermission();
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
-        mMap.setOnMyLocationButtonClickListener(this);
-
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng point) {
-                mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(point));
-            }
-        });
-    }
-
-    @Override
-    public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-        return false;
-    }
-
-    public void askForLocationPermission(){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
-        }
-        else if (mMap != null) {
-            mMap.setMyLocationEnabled(true);
-            mFusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                latitude = location.getLatitude();
-                                longitude = location.getLongitude();
-                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 15));
-
-                                mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)));
-                            }
-                        }
-                    });
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case LOCATION_PERMISSION_REQUEST_CODE: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    askForLocationPermission();
-                } else {
-                    Toast.makeText(this, "Nms necesitamos tu ubicacion puto",
-                            Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-
-        }
-    }
 }
---
+++ Android/app/src/main/java/com/alas/balet/com/alas/ListAdapters/ParkingsAdapter.java
@@ -0,0 +1,49 @@
+package com.alas.balet.com.alas.ListAdapters;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alas.balet.R;
+import com.squareup.picasso.Picasso;
+
+public class ParkingsAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] names;
+    private final String[] descriptions;
+    private final String[] images;
+
+    public ParkingsAdapter(Activity context, String[] names, String[] images, String[] descriptions) {
+        super(context, R.layout.parking_list, names);
+        // TODO Auto-generated constructor stub
+
+        this.context = context;
+        this.names = names;
+        this.images = images;
+        this.descriptions = descriptions;
+    }
+
+
+    public View getView(int position, View view, ViewGroup parent) {
+
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.parking_list, null,true);
+
+        TextView drivers_name = rowView.findViewById(R.id.drivers_name);
+        ImageView drivers_image = rowView.findViewById(R.id.drivers_image);
+        TextView drivers_description = rowView.findViewById(R.id.drivers_description);
+
+        drivers_name.setText(names[position]);
+        Picasso.get()
+                .load(images[position])
+                .into(drivers_image);
+        drivers_description.setText(descriptions[position]);
+        return rowView;
+
+    };
+}
